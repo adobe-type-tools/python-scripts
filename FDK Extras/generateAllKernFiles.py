2718e1ba350db5f8b6d593da749d80569ad5f4cf
Miguel Sousa <msousa@adobe.com>
2015-10-12 04:08:20 -0700

    white space

--- FDK Extras/generateAllKernFiles.py
+++ FDK Extras/generateAllKernFiles.py
@@ -12,28 +12,28 @@ writeSubtables = False  # Sometimes the kerning feature file needs to have expli
 
 __copyright__ = __license__ =  """
 Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
- 
+
 Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation 
-the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Software, and to permit persons to whom the 
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
- 
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
- 
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
 __doc__ = """
-This script takes a path to a folder as input, finds all the UFOs inside that folder 
+This script takes a path to a folder as input, finds all the UFOs inside that folder
 and its subdirectories, and outputs each font's kerning in feature file syntax.
 If a path is not provided, the script uses the current path as the top-most directory.
 The name of the resulting kerning FEA file is managed by the WriteFeaturesKernFDK module.
@@ -86,7 +86,7 @@ def doTask(fonts):
 
 		print '*******************************'
 		print 'Kerning for %s...(%d/%d)' % (styleName, i, totalFonts)
-		
+
 		ufoFont = Font(fontFileName)
 		WriteFeaturesKernFDK.KernDataClass(ufoFont, folderPath, minKern, writeTrimmed, writeSubtables)
 
@@ -111,7 +111,7 @@ def run():
 	t1 = time.time()
 
 	getFontPaths(baseFolderPath, baseFolderPath)
-		
+
 	if len(fontsList):
 		doTask(fontsList)
 	else:
@@ -120,7 +120,7 @@ def run():
 
 	t2 = time.time()
 	elapsedSeconds = t2-t1
-	
+
 	if (elapsedSeconds/60) < 1:
 		print 'Completed in %.1f seconds.' % elapsedSeconds
 	else:
